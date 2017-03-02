675627d3d1d9a85842abdf0dc12d868b616187ed
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2017-03-03 01:13:31 +0900

    Remove redux-devtools and import redux-logger dynamically

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,31 +1,24 @@
+/* eslint-disable global-require */
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
-import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
-const loggerMiddleware = createLogger();
 // eslint-disable-next-line no-underscore-dangle
 const __PRODUCTION__ = process.env.NODE_ENV === 'production';
 
 export default function configureStore(initialState) {
-  const middlewares = [loggerMiddleware];
+  const middlewares = [];
   if (!__PRODUCTION__) {
-    middlewares.push(loggerMiddleware);
+    const createLogger = require('redux-logger');
+    middlewares.push(createLogger());
   }
-  const middlewareEnhancer = applyMiddleware(...middlewares);
-
-  const storeEnhancers = [middlewareEnhancer];
-  const composedEnhancer = composeWithDevTools(...storeEnhancers);
-
   const store = createStore(
     rootReducer,
     initialState,
-    composedEnhancer,
+    applyMiddleware(...middlewares),
   );
 
   if (!__PRODUCTION__ && module.hot) {
     module.hot.accept('../reducers', () => {
-      // eslint-disable-next-line global-require
       const newRootReducer = require('../reducers').default;
       store.replaceReducer(newRootReducer);
     });
